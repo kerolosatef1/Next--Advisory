89da38c33afef9009fba0094f198303489bbaa51
kerolosatef1 <atefkerolos25@gmail.com>
2025-05-22 19:14:59 +0300

    Done

--- src/components/Slidebar/Slidebar.jsx
+++ src/components/Slidebar/Slidebar.jsx
@@ -191,6 +191,13 @@ const Slidebar = () => {
                     <button onClick={() => navigate('/hall-schedule')}>Halls Schedule</button>
 
                   </ListItem>
+                  <ListItem>
+                    <ListItemPrefix>
+                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                    </ListItemPrefix>
+                    <button onClick={() => navigate('/professor-schedule')}>Professor Schedule</button>
+
+                  </ListItem>
                 </List>
               </AccordionBody>
             </Accordion>
