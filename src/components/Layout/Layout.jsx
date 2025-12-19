963e90903ee5a8603d03264ebd3c1a0ffa9b39c8
kerolosatef1 <atefkerolos25@gmail.com>
2025-12-19 17:41:26 +0200

    Done

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Style from "./Layout.module.css";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./../Navbar/Navbar";
 import Footer from "./../Footer/Footer";
